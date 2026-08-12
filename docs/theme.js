74dc3ddaace1614df144c57ea9cdc6479508b383
Scott Meddings <scott@scottmeddings.com>
2026-08-12 21:31:29 +1000

    Add Power Management to shared site navigation

--- docs/theme.js
+++ docs/theme.js
@@ -25,6 +25,7 @@
     ensureLink('/GPTars_v2/software', 'Software');
     ensureLink('/GPTars_v2/personality', 'Personality');
     ensureLink('/GPTars_v2/lessons', 'Lessons');
+    ensureLink('/GPTars_v2/power', 'Power Management');
   }
 
   // Add the full architecture drawing to the Software page. Keeping this in the
